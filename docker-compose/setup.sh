d9c8d598371dbd5cd433c9b1b265fc50167d1c22
Raffaele Ragni <raffaele.ragni@gmail.com>
2022-05-27 23:36:16 +0200

    support for non-automated local environment

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -2,7 +2,13 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 <<-EOSQL
+  CREATE DATABASE api;
   CREATE DATABASE api_test;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 -d api <<-EOSQL
+  CREATE EXTENSION IF NOT EXISTS "postgis";
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
 
 psql -v ON_ERROR_STOP=1 -d api_test <<-EOSQL
